cfc60868b834e2fd11a425c1e5ae71875f891fdc
Yehor Kotsiuba <kotsiyeh@fit.cvut.cz>
2023-09-14 21:38:37 +0200

    feat: Wrapp json history reading in database

--- app/app.py
+++ app/app.py
@@ -1,15 +1,49 @@
 import json
+import psycopg2
+import os
+from dotenv import load_dotenv
 
-with open("../data/watch-history.json") as history:
-    data = json.load(history)
+def main():
+    load_dotenv()
 
-    for watch_record in data:
-        video_id = watch_record["titleUrl"].split("=")[-1]
-        video_name = watch_record["title"]
+    conn = None
+    cursor = None
 
-        chanel_id = watch_record["subtitles"][0]["url"].split('/')[-1]
-        chanel_name = watch_record["subtitles"][0]["name"]
+    try:
+        conn = psycopg2.connect(
+            host=os.environ.get("DB_HOST"),
+            dbname=os.environ.get("DB_NAME"),
+            user=os.environ.get("DB_USER"),
+            password=os.environ.get("DB_PASSWORD"),
+            port=os.environ.get("DB_PORT")
+        )
 
-        watch_time = watch_record["time"]
+        cursor = conn.cursor()
 
-        print(video_id, video_name, chanel_id, chanel_name, watch_time)
+        with open("../data/watch-history.json") as history:
+            data = json.load(history)
+
+            for watch_record in data:
+                video_id = watch_record["titleUrl"].split("=")[-1]
+                video_name = watch_record["title"]
+
+                chanel_id = watch_record["subtitles"][0]["url"].split('/')[-1]
+                chanel_name = watch_record["subtitles"][0]["name"]
+
+                watch_time = watch_record["time"]
+
+                print(video_id, video_name, chanel_id, chanel_name, watch_time)
+
+                conn.commit()
+
+    except Exception as error:
+        print(error)
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if conn is not None:
+            conn.close()
+
+
+if __name__ == "__main__":
+    main()
