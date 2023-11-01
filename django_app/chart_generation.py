874c38a079a7b1060a07d423696d2fda9107c164
Yehor Kotsiuba <kotsiyeh@fit.cvut.cz>
2023-11-02 00:14:24 +0100

    feat: Copy df in chart generation functions

--- django_app/chart_generation.py
+++ django_app/chart_generation.py
@@ -5,6 +5,9 @@ import plotly.express as px
 def category_trend_chart(df):
     """Watched videos by categories for all period"""
 
+    # Copy df because we will change it
+    df = df.copy()
+
     # Floor time to day
     df['time'] = df['time'].dt.floor('d')
 
@@ -30,10 +33,12 @@ def category_trend_chart(df):
 def category_total_watched_chart(df):
     """Bar chart by total amount of watched videos by category"""
 
+    # Copy df because we will change it
+    df = df.copy()
+
     # Group by category and count. Only category name and count columns
     df = df.groupby('video__category__name').size().to_frame('nr_of_watched_videos').reset_index()
 
-    # Plotly bar chart
     fig = px.bar(
         df,
         x='video__category__name',
