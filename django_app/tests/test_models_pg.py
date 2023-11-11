540a64a2c43104c1db2af80312d492d0fc89e39f
Yehor Kotsiuba <kotsiyeh@fit.cvut.cz>
2023-11-11 11:02:27 +0100

    test: Add tests for pg models relations

---
+++ django_app/tests/test_models_pg.py
@@ -0,0 +1,83 @@
+from ..models.postgres import (WatchRecord, Video,
+                               Category, Chanel, UserProfile)
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+
+class TestPostgresModelsRelations(TestCase):
+    """Test models relations."""
+
+    def setUp(self):
+        """Set up test data."""
+        self.user = User.objects.create_user(username='test_user',
+                                             password='12345')
+        self.user_profile = UserProfile.objects.create(user_id=self.user)
+        self.category = Category.objects.create(name='test_category')
+        self.chanel = Chanel.objects.create(custom_id='test_chanel',
+                                            name='test_chanel')
+        self.video = Video.objects.create(custom_id='test_video',
+                                          name='test_video',
+                                          chanel=self.chanel,
+                                          category=self.category)
+        self.watch_record = WatchRecord.objects.create(
+            time='2033-01-01 00:00:00+00:00',
+            user_profile=self.user_profile,
+            video=self.video
+        )
+
+    def test_user_profile_userid_is_user(self):
+        """Test UserProfile user_id is related to User."""
+        self.assertEqual(self.user_profile.user_id, self.user)
+
+    def test_watch_record_foreign_keys_relations(self):
+        """Test WatchRecord user_profile relate to UserProfile and video relate to Video."""
+        self.assertEqual(self.watch_record.user_profile, self.user_profile)
+        self.assertEqual(self.watch_record.video, self.video)
+
+    def test_video_foreign_keys_relations(self):
+        """Test Video chanel relate to Chanel and category relate to Category."""
+        self.assertEqual(self.video.chanel, self.chanel)
+        self.assertEqual(self.video.category, self.category)
+
+    def test_watch_record_uniqueness(self):
+        """Test WatchRecord uniqueness."""
+        time = self.watch_record.time
+        user_profile = self.watch_record.user_profile
+        video = self.watch_record.video
+
+        with self.assertRaises(Exception):
+            WatchRecord.objects.create(
+                time=time,
+                user_profile=user_profile,
+                video=video
+            )
+
+    def test_watch_record_same_but_different_time(self):
+        """WatchRecord must be created if same record exists but with different time."""
+        time = '2034-01-01 00:00:00+00:00'
+        user_profile = self.watch_record.user_profile
+        video = self.watch_record.video
+
+        try:
+            WatchRecord.objects.create(
+                time=time,
+                user_profile=user_profile,
+                video=video
+            )
+        except Exception:
+            self.fail('WatchRecord with same user_profile and video but different time should be created.')
+
+    def test_watch_record_join_with_video_chanel_category(self):
+        """Test WatchRecord join with Video, Chanel and Category.
+        Check if joined data is correct."""
+        watch_records = WatchRecord.objects.filter(user_profile=self.user_profile)
+
+        watch_records = watch_records.select_related('video')
+        watch_records = watch_records.select_related('video__chanel')
+        watch_records = watch_records.select_related('video__category')
+
+        # Check if joined data is correct
+        self.assertEqual(watch_records[0].video, self.video)
+        self.assertEqual(watch_records[0].video.chanel, self.chanel)
+        self.assertEqual(watch_records[0].video.category, self.category)
+
