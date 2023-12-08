2300b85c2c59dc4a7e242260e1b33e4df7752daf
Yehor Kotsiuba <kotsiyeh@fit.cvut.cz>
2023-12-08 22:42:26 +0100

    other: Django secret key using env variable

--- youtube_analyzer/settings.py
+++ youtube_analyzer/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+7@&dtl+5jq_9cr8fa!3kbzehur_aief7*uv7&6^k^@h6n^xhp'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
